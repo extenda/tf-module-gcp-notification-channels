d6e2ed1754f90e0959f530f734d4fc7cbac9f53e
Anastasiia Kovtun <nastia@Anastasiias-MacBook-Pro-2.local>
2020-08-13 15:19:52 +0300

    feat: Add notification channels module

---
+++ providers.tf
@@ -0,0 +1,11 @@
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  required_version = ">= 0.12.18"
+  required_providers {
+    google      = ">= 3.8"
+  }
+}
+
+provider "google" {
+  version = ">= 3.8"
+}
