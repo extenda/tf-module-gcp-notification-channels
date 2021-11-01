eecfaf6e80d649db4d7c40816bb31d17a75d008d
daseg <david.seglert@extendaretail.com>
2021-11-01 15:45:10 +0100

    allow list input to create multiple slack channels

--- main.tf
+++ main.tf
@@ -14,12 +14,14 @@ resource "google_monitoring_notification_channel" "clan_email" {
 }
 
 resource "google_monitoring_notification_channel" "clan_slack_channel" {
-  count        = var.clan_slack_channel != "" ? 1 : 0
+  for_each = var.clan_slack_channel
+  
+  count        = each.value != "" ? 1 : 0
   project      = var.tribe_project_id
   display_name = "Notification Slack channel for ${var.clan_name} clan"
   type         = "slack"
   labels = {
-    channel_name = var.clan_slack_channel
+    channel_name = each.value
   }
   sensitive_labels {
     auth_token = data.google_secret_manager_secret_version.monitoring_slack_token.secret_data
