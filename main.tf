580396fd06aa9af0cc3debe26d57f0c2099ad8fb
Anastasiia Kovtun <57349964+akovtun-extenda@users.noreply.github.com>
2022-01-11 14:35:12 +0200

    fix: Update display name (#4)

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ data "google_secret_manager_secret_version" "monitoring_slack_token" {
 resource "google_monitoring_notification_channel" "clan_email" {
   count        = var.clan_group_email != "" ? 1 : 0
   project      = var.tribe_project_id
-  display_name = "Notification Email channel for ${var.clan_name} clan"
+  display_name = "Email channel for ${var.clan_name} clan"
   type         = "email"
   labels = {
     email_address = var.clan_group_email
@@ -20,7 +20,7 @@ resource "google_monitoring_notification_channel" "clan_slack_channels" {
   }
   
   project      = var.tribe_project_id
-  display_name = "Notification Slack channel for ${var.clan_name} clan"
+  display_name = "Slack channel for ${var.clan_name} clan: ${each.value.name}"
   type         = "slack"
   labels = {
     channel_name = each.value.name
